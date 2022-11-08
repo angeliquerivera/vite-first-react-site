63fbdb2480124b51198307252b5f2bf3c6c8c1fe
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-11-07 20:42:34 -0500

    fix: accept incoming changes in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,37 +4,7 @@ export default function Hero() {
   return (
     <section className="hero is-fullheight">
       <div className="hero-head">
-<<<<<<< HEAD
-        <header className="navbar is-fixed-top">
-          <div className="container">
-            <div className="navbar-brand">
-              <a className="navbar-item" href="/#">
-                <img src="https://bulma.io/images/bulma-logo.png" alt="Logo" />
-              </a>
-              <span className="navbar-burger" data-target="navbarMenuHeroC">
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
-            </div>
-            <div id="navbarMenuHeroC" className="navbar-menu">
-              <div className="navbar-end">
-                <a className="navbar-item is-active" href="/eyes">
-                  Eyes
-                </a>
-                <a className="navbar-item" href="/lips">
-                  Lips
-                </a>
-                <a className="navbar-item" href="/face">
-                  Face
-                </a>
-              </div>
-            </div>
-          </div>
-        </header>
-=======
         <Navbar />
->>>>>>> feat/1-add-brand-logo-to-nav-bar
       </div>
 
       <div className="hero-body">
