99ef9026359a694f5425f1b5a24c07e7c97fb57f
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-11-09 20:04:41 -0500

    feat: add code for hamburger menu active toggle
    Resolves: #3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import brandLogo from "../assets/images/makeup-logo.png";
 
 export default function Navbar() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <header className="navbar is-fixed-top">
       <div className="container">
@@ -8,15 +10,26 @@ export default function Navbar() {
           <a className="navbar-item" href="/#">
             <img src={brandLogo} alt="Logo" />
           </a>
-          <span className="navbar-burger" data-target="navbarMenuHeroC">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
+          <a
+            role="button"
+            className={isActive ? "navbar-burger is-active" : "navbar-burger"}
+            aria-label="menu"
+            aria-expanded="false"
+            onClick={() => {
+              setIsActive((state) => !state);
+            }}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
-        <div id="navbarMenuHeroC" className="navbar-menu">
+        <div
+          id="navbarMenuHeroC"
+          className={isActive ? "navbar-menu is-active" : "navbar-menu"}
+        >
           <div className="navbar-end">
-            <a className="navbar-item is-active" href="/eyes">
+            <a className="navbar-item" href="/eyes">
               Eyes
             </a>
             <a className="navbar-item" href="/lips">
