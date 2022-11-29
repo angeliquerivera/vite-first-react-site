7cfb34490da08c6efdd031f8c32fa18a8429780f
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-11-28 19:21:05 -0500

    add collections images to MakeupCollections component
    Refs: #12

--- src/components/MakeupCollections.jsx
+++ src/components/MakeupCollections.jsx
@@ -1,7 +1,36 @@
+import collectionOne from "../assets/images/collection-1.jpg";
+import collectionTwo from "../assets/images/collection-2.jpg";
+import collectionThree from "../assets/images/collection-3.jpg";
+import collectionFour from "../assets/images/collection-4.jpg";
+
 export default function MakeupCollections() {
   return (
     <section className="section is-large collections">
-      <h1 className="title">Find Your Diva Look</h1>
+      <h1 className="title">Collections For Your Inner Diva</h1>
+      <div className="columns">
+        <div className="column">
+          <figure className="image is-4by3">
+            <img src={collectionOne} />
+          </figure>
+          <div />
+
+          <div className="column">
+            <figure className="image is-4by3">
+              <img src={collectionTwo} />
+            </figure>
+          </div>
+          <div className="column">
+            <figure className="image is-4by3">
+              <img src={collectionThree} />
+            </figure>
+          </div>
+          <div className="column">
+            <figure className="image is-4by3">
+              <img src={collectionFour} />
+            </figure>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
