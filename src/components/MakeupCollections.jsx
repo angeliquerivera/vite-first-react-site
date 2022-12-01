38438d04ac4bd1c73d75aa02eac2d3ae0086205b
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-11-30 19:49:49 -0500

    fix: modify alignment for image text and buttons
    Refs: #12

--- src/components/MakeupCollections.jsx
+++ src/components/MakeupCollections.jsx
@@ -5,37 +5,51 @@ import collectionFour from "../assets/images/collection-4.jpg";
 
 export default function MakeupCollections() {
   return (
-    <section className="section is-large collections">
+    <section className="section is-medium collections">
       <h1 className="title">Collections For Your Inner Diva</h1>
       <div className="columns">
         <div className="column">
           <figure className="image">
             <img src={collectionOne} />
           </figure>
-          <h1 className="palette-name">Shine Bright</h1>
-          <p className="price">$20</p>
+          <div className="collection-details">
+            <h1 className="palette-name">Shine Bright</h1>
+            <p className="price">$20</p>
+            <button className="buy button is-info is-active">Buy</button>
+          </div>
         </div>
 
         <div className="column">
           <figure className="image">
             <img src={collectionTwo} />
           </figure>
-          <h1 className="palette-name">OmniLook</h1>
-          <p className="price">$40</p>
+          <div className="collection-details">
+            <h1 className="palette-name">OmniLook</h1>
+            <p className="price">$40</p>
+            <button className="buy button is-info is-active">Buy</button>
+          </div>
         </div>
+
         <div className="column">
           <figure className="image">
             <img src={collectionThree} />
           </figure>
-          <h1 className="palette-name">Shimmer 'n Simmer</h1>
-          <p className="price">$20</p>
+          <div className="collection-details">
+            <h1 className="palette-name">Shimmer 'n Simmer</h1>
+            <p className="price">$20</p>
+            <button className="buy button is-info is-active">Buy</button>
+          </div>
         </div>
+
         <div className="column">
           <figure className="image">
             <img src={collectionFour} />
           </figure>
-          <h1 className="palette-name">Teen Drama</h1>
-          <p className="price">$15</p>
+          <div className="collection-details">
+            <h1 className="palette-name">Teen Drama</h1>
+            <p className="price">$15</p>
+            <button className="buy button is-info is-active">Buy</button>
+          </div>
         </div>
       </div>
     </section>
