e4099fee157faed71e397c6dac3f90a22c949413
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-12-07 20:33:42 -0500

    feat: add logos to Featured In component
    Refs: #18

--- src/components/FeaturedIn.jsx
+++ src/components/FeaturedIn.jsx
@@ -1,7 +1,34 @@
+import Glamour from "../assets/logos/glamour-logo.png";
+import Elle from "../assets/logos/elle-logo.png";
+import Allure from "../assets/logos/allure-logo.png";
+import Vogue from "../assets/logos/vogue-logo.png";
+
 export default function FeaturedIn() {
   return (
     <section className="section is-medium featured-in">
       <h1 className="title">Featured In</h1>
+      <div className="columns">
+        <div className="column">
+          <figure className="image">
+            <img src={Glamour} />
+          </figure>
+        </div>
+        <div className="column">
+          <figure className="image">
+            <img src={Elle} />
+          </figure>
+        </div>
+        <div className="column">
+          <figure className="image">
+            <img src={Allure} />
+          </figure>
+        </div>
+        <div className="column">
+          <figure className="image">
+            <img src={Vogue} />
+          </figure>
+        </div>
+      </div>
     </section>
   );
 }
