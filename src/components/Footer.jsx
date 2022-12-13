7cc2198eea79a822ff43559448a3814d87f6849c
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-12-12 20:54:12 -0500

    add icons and import fort awesome icons to Footer component
    Refs: #19

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faTiktok,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
 export default function Footer() {
   return (
     <footer class="footer">
       <div class="content has-text-centered">
-        <p>
-          <strong>Bulma</strong> by{" "}
-          <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is
-          licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
-          website content is licensed{" "}
-          <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-            CC BY NC SA 4.0
-          </a>
-          .
-        </p>
+        <div className="columns is-centered">
+          <div className="column is-narrow">
+            <span className="icon is-large">
+              <FontAwesomeIcon icon={faFacebook} size="4x" />
+            </span>
+          </div>
+          <div className="column is-narrow">
+            <span className="icon is-large">
+              <FontAwesomeIcon icon={faInstagram} size="4x" />
+            </span>
+          </div>
+          <div className="column is-narrow">
+            <span className="icon is-large">
+              <FontAwesomeIcon icon={faTwitter} size="4x" />
+            </span>
+          </div>
+          <div className="column is-narrow">
+            <span className="icon is-large">
+              <FontAwesomeIcon icon={faTiktok} size="4x" />
+            </span>
+          </div>
+          <div className="column is-narrow">
+            <span className="icon is-large">
+              <FontAwesomeIcon icon={faYoutube} size="4x" />
+            </span>
+          </div>
+        </div>
       </div>
     </footer>
   );
