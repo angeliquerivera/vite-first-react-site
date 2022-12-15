04880c616da1cda9e6dd048ee4f6ab28efac1c83
Angelique Rivera <angeliqueriveramorales@gmail.com>
2022-12-14 21:49:26 -0500

    feat: change link color to match icon color
    Refs: #19

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,27 +12,27 @@ export default function Footer() {
     <footer class="footer">
       <div class="content has-text-centered">
         <div className="columns is-centered">
-          <div className="column is-narrow">
+          <div className="column icon-column is-narrow">
             <span className="icon is-large">
               <FontAwesomeIcon icon={faFacebook} size="4x" />
             </span>
           </div>
-          <div className="column is-narrow">
+          <div className="column icon-column is-narrow">
             <span className="icon is-large">
               <FontAwesomeIcon icon={faInstagram} size="4x" />
             </span>
           </div>
-          <div className="column is-narrow">
+          <div className="column icon-column is-narrow">
             <span className="icon is-large">
               <FontAwesomeIcon icon={faTwitter} size="4x" />
             </span>
           </div>
-          <div className="column is-narrow">
+          <div className="column icon-column is-narrow">
             <span className="icon is-large">
               <FontAwesomeIcon icon={faTiktok} size="4x" />
             </span>
           </div>
-          <div className="column is-narrow">
+          <div className="column icon-column is-narrow">
             <span className="icon is-large">
               <FontAwesomeIcon icon={faYoutube} size="4x" />
             </span>
